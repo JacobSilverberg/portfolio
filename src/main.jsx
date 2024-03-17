93f7063840399b361538c0d3a64bf94d4c0b9a55
Jacob Silverberg <minkus962@gmail.com>
2024-03-17 17:28:33 -0400

    project content

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom/client'
 import { Hero } from './components/Hero.jsx'
 import { About } from './components/About.jsx'
 import { Skills } from './components/Skills.jsx'
-// import { Separator } from './components/Separator.jsx'
 import { Break } from './components/Break.jsx'
+import { Projects } from './components/Projects.jsx'
 import './output.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Hero />
-    <Break mt={0} mb={24} />
+    <Break />
     <About />
     <Break />
     <Skills />
+    <Break />
+    <Projects />
   </React.StrictMode>,
 )
